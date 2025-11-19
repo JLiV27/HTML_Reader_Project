6c5369892a9e45013061a50a64c80130d7abf61b
JLiVigni27 <jace_livigni27@milton.edu>
2025-11-19 08:54:54 -0500

    Actual Finall Version

--- src/SwingControlDemo.java
+++ src/SwingControlDemo.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -18,23 +17,28 @@ public class SwingControlDemo implements ActionListener {
     private JTextArea taLink; //typing area
     private JTextArea taSearch;
     private JTextArea taOutput;
-    private int WIDTH=800;
-    private int HEIGHT=700;
+    private final int WIDTH=800;
+    private final int HEIGHT=700;
     JButton startButton = new JButton("Start");
 
     public String readOutput;
     public String[] manyTerms;
+    public String[] singleReferenceLinks;
+    public String[] doubleReferenceLinks;
+    public String[] singleSourceLinks;
+    public String[] doubleSourceLinks;
+    public String finalOutput;
 
-    public SwingControlDemo() {
+    public SwingControlDemo() { //calls prepare gui when program starts
         prepareGUI();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //calls showeventdemo
         SwingControlDemo swingControlDemo = new SwingControlDemo();
         swingControlDemo.showEventDemo();
     }
 
-    private void prepareGUI() {
+    private void prepareGUI() { //method runs once when program starts and sets up mainframe and toolbar
         mainFrame = new JFrame("Java SWING Examples");
         mainFrame.setSize(WIDTH, HEIGHT);
         mainFrame.setLayout(new GridLayout(1, 1));
@@ -71,13 +75,13 @@ public class SwingControlDemo implements ActionListener {
         mainFrame.setVisible(true);
     }
 
-    private void showEventDemo() {
+    private void showEventDemo() { //runs once when program starts and puts together starting gui
 
         startButton.setActionCommand("Start");
 
         startButton.addActionListener(new ButtonClickListener());
 
-        mainFrame.add(startButton);
+        mainFrame.add(startButton); //when program starts, adds massive start button to begin the program
 
         mainFrame.setVisible(true);
     }
@@ -94,23 +98,19 @@ public class SwingControlDemo implements ActionListener {
             taLink.selectAll();
     }
 
-    //private void jButtonActionPerformed(java.awt.event.ActionEvent evt){
-    //JOptionPane.showMessageDialog(JRootPane, taSearch.getText());
-    //}
-
     private class ButtonClickListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e) { //method to take command set by button listener and execute an action based off it
             String command = e.getActionCommand();
 
-            if (command.equals("Start")) {
-                mainFrame.remove(startButton);
+            if (command.equals("Start")) { //Once start of the program start button is pressed, sets up new GUI for all the input boxes and Read HTML button
+                mainFrame.remove(startButton); //first removes the giant start button to show the rest of the gui
 
-                mainFrame.setLayout(new GridLayout(2,1));
+                mainFrame.setLayout(new GridLayout(2,1)); //changes grid layout of mainframe
 
                 controlPanel = new JPanel();
                 controlPanel.setLayout(new GridLayout(1,3)); //set the layout of the pannel
 
-                taOutput = new JTextArea("Waiting for input...");
+                taOutput = new JTextArea("Waiting for input..."); //creates JText Area Object
                 taOutput.setBounds(50, 5, WIDTH - 100, HEIGHT - 50);
 
                 JScrollPane scrollPane = new JScrollPane(taOutput,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
@@ -119,107 +119,78 @@ public class SwingControlDemo implements ActionListener {
                 mainFrame.add(controlPanel);
                 mainFrame.add(scrollPane);
 
-                taLink = new JTextArea("https://www.nytimes.com/");
+                taLink = new JTextArea("Input a link to a website for its HTML to be parsed for links.\nYou can also search for specific terms"); //creates JText Area Object
                 taLink.setBounds(50, 5, WIDTH - 100, HEIGHT - 50);
 
                 JButton readButton = new JButton("Read HTML");
                 readButton.setActionCommand("Read");
-                readButton.addActionListener(new ButtonClickListener());
+                readButton.addActionListener(new ButtonClickListener()); //adds a listener to whenever the button is clicked
 
-                taSearch = new JTextArea("Input search terms separated\nby commas with no spaces");
+                taSearch = new JTextArea("Input search terms separated\nby commas with no spaces"); //creates JText Area Object
                 taSearch.setBounds(50,5,WIDTH-100,HEIGHT-100);
 
                 mainFrame.add(mb);  //add menu bar
                 controlPanel.add(taLink);//add typing area
-                controlPanel.add(readButton);
-                controlPanel.add(taSearch);
+                controlPanel.add(readButton); //adds button to read HTML
+                controlPanel.add(taSearch); //adds typing area to identify search terms
                 mainFrame.setJMenuBar(mb); //set menu bar
                 mainFrame.setVisible(true);
             }
-            else if(command.equals("Read")) {
+            else if(command.equals("Read")) { //if Read HTML button is clicked it sends this command and triggers process
 
-                //String searchTerm = taSearch.getText();
+                manyTerms = (taSearch.getText() + ",").split(","); //gets the search terms from the input search term box, adds comma at the end
+                // so as to not get null return for trying to split a string's commas for a string that doesnt have any commas
 
-                if (taSearch.getText().contains(",")) {
-                    manyTerms = taSearch.getText().split(",");
-                }
-
-                String linkTerm = taLink.getText();
-
-                URL url = null;
-                try {
-                    url = new URL(linkTerm);
-                } catch (MalformedURLException ex) {
-                    throw new RuntimeException(ex);
-                }
-
-                URLConnection urlc = null;
-                try {
-                    urlc = url.openConnection();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-                urlc.setRequestProperty("User-Agent", "Mozilla 5.0 (Windows; U; " + "Windows NT 5.1; en-US; rv:1.8.0.11) ");
-
-                BufferedReader reader = null;
-                try {
-                    reader = new BufferedReader(
-                            new InputStreamReader(urlc.getInputStream())
-                    );
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
+                BufferedReader reader = getBufferedReader();
                 String line;
-                while (true) {
+                while (true) { //while loop that checks each line of the html code
                     try {
                         if ((line = reader.readLine()) == null) break;
                     } catch (IOException ex) {
                         throw new RuntimeException(ex);
                     }
-                    for (int x = 0; x < manyTerms.length; x++) {
-                        if (line.contains(manyTerms[x])) {
-                            if (line.contains(" src=\"")) {
-                                String[] doubleSourceLinks = line.split("src=\"");
-                                for (int i = 1; i < doubleSourceLinks.length; i++) {
-                                    System.out.println(line);
-                                    doubleSourceLinks[i] = doubleSourceLinks[i].substring(0, doubleSourceLinks[i].indexOf("\""));
-                                    System.out.println(doubleSourceLinks[i]);
-                                    readOutput += doubleSourceLinks[i] + "\n";
-                                }
-                            }
-
-                            if (line.contains(" src='")) {
-                                String[] singleSourceLinks = line.split("src='");
-                                for (int i = 1; i < singleSourceLinks.length; i++) {
-                                    System.out.println(line);
-                                    singleSourceLinks[i] = singleSourceLinks[i].substring(0, singleSourceLinks[i].indexOf("'"));
-                                    System.out.println(singleSourceLinks[i]);
-                                    readOutput += singleSourceLinks[i] + "\n";
-                                }
-                            }
-
-                            if (line.contains(" href=\"")) {
-                                String[] doubleReferenceLinks = line.split("href=\"");
-                                for (int i = 1; i < doubleReferenceLinks.length; i++) {
-                                    System.out.println(line);
-                                    doubleReferenceLinks[i] = doubleReferenceLinks[i].substring(0, doubleReferenceLinks[i].indexOf("\""));
-                                    System.out.println(doubleReferenceLinks[i]);
-                                    readOutput += doubleReferenceLinks[i] + "\n";
-                                }
-                            }
-
-                            if (line.contains(" href='")) {
-                                String[] singleReferenceLinks = line.split("href='");
-                                for (int i = 1; i < singleReferenceLinks.length; i++) {
-                                    System.out.println(line);
-                                    singleReferenceLinks[i] = singleReferenceLinks[i].substring(0, singleReferenceLinks[i].indexOf("'"));
-                                    System.out.println(singleReferenceLinks[i]);
-                                    readOutput += singleReferenceLinks[i] + "\n";
-                                }
-                            }
-                            taOutput.setText(readOutput);
+                    if (line.contains(" src=\"")) {
+                        doubleSourceLinks = line.split("src=\"");
+                        for (int i = 1; i < doubleSourceLinks.length; i++) {
+                            doubleSourceLinks[i] = doubleSourceLinks[i].substring(0, doubleSourceLinks[i].indexOf("\"")); //shortens each shortened string to the first instance of a double quote which is where the link ends
+                            readOutput += doubleSourceLinks[i] + "\n"; //assigns the found link to the eventual output which is later checked if it contains a given search term
                         }
                     }
+
+                    if (line.contains(" src='")) {
+                        singleSourceLinks = line.split("src='");
+                        for (int i = 1; i < singleSourceLinks.length; i++) {
+                            singleSourceLinks[i] = singleSourceLinks[i].substring(0, singleSourceLinks[i].indexOf("'")); //shortens each shortened string to the first instance of a single quote which is where the link ends
+                            readOutput += singleSourceLinks[i] + "\n"; //assigns the found link to the eventual output which is later checked if it contains a given search term
+                        }
+                    }
+
+                    if (line.contains(" href=\"")) {  //checks if a given line contains an href" link
+                        doubleReferenceLinks = line.split("href=\""); //splits the line into multiple strings at each instance of an href
+                        for (int i = 1; i < doubleReferenceLinks.length; i++) {
+                            doubleReferenceLinks[i] = doubleReferenceLinks[i].substring(0, doubleReferenceLinks[i].indexOf("\"")); //shortens each shortened string to the first instance of a double quote which is where the link ends
+                            readOutput += doubleReferenceLinks[i] + "\n"; //assigns the found link to the eventual output which is later checked if it contains a given search term
+                        }
+                    }
+
+                    if (line.contains(" href='")) {
+                        singleReferenceLinks = line.split("href='");
+                        for (int i = 1; i < singleReferenceLinks.length; i++) {
+                            singleReferenceLinks[i] = singleReferenceLinks[i].substring(0, singleReferenceLinks[i].indexOf("'"));
+                            readOutput += singleReferenceLinks[i] + "\n";
+                        }
+                    }
+                    taOutput.setText(readOutput); //sets all links to readoutput so readoutput is not considered null (dont know why that was an issue)
+
+                    String[] termCheck = taOutput.getText().split("\n"); //splits each link back into individual lines to be searched 1 by 1
+                    for (int i = 0; i < manyTerms.length; i++) { //runs for loop for every search term identified
+                        for (int j = 0; j < termCheck.length; j++) { //checks every line for a given search term
+                            if(termCheck[j].contains(manyTerms[i])){
+                                finalOutput += termCheck[j] + "\n";
+                            }
+                        }
+                    }
+                    taOutput.setText(finalOutput);
                 }
                 try {
                     reader.close();
@@ -231,6 +202,35 @@ public class SwingControlDemo implements ActionListener {
                     System.out.println(ex);
                 }
             }
+        }
+
+        private BufferedReader getBufferedReader() { //method to actually connect to the website provided by the link
+            String linkTerm = taLink.getText();
+
+            URL url = null;
+            try {
+                url = new URL(linkTerm);
+            } catch (MalformedURLException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            URLConnection urlc = null;
+            try {
+                urlc = url.openConnection();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            urlc.setRequestProperty("User-Agent", "Mozilla 5.0 (Windows; U; " + "Windows NT 5.1; en-US; rv:1.8.0.11) ");
+
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(
+                        new InputStreamReader(urlc.getInputStream())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            return reader;
         }
     }
 }
